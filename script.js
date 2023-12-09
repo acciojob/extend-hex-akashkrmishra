dacaeef69772a0be09dfa3766e2f883422eeea94
akashkrmishra <142215838+akashkrmishra@users.noreply.github.com>
2023-12-09 17:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	if(shortHex==4){
+		let ans=`#${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}${shortHex[3]}${shortHex[3]}`;
+		console.log(ans);
+	}else{
+		let ans=`#${shortHex[0]}${shortHex[0]}${shortHex[1]}${shortHex[1]}${shortHex[2]}${shortHex[2]}`;
+		console.log(ans);
+	}
 };
 
 // Do not change the code below.
